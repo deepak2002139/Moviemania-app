cbaf03d3dc992f94537ff838b4bede1775fb0f86
Deepak Kumar <99772696+deepak2002139@users.noreply.github.com>
2023-04-11 13:08:10 +0530

    Add files via upload

---
+++ src/components/Genre.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useContext } from 'react'
+import Contextpage from '../Contextpage';
+import {Helmet} from "react-helmet";
+
+function Genre() {
+    const { fetchGenre, activegenre, setActiveGenre, genres } = useContext(Contextpage);
+
+    useEffect(() => {
+        fetchGenre();
+    }, [])
+
+    // const filterFunc = () => {
+    //     if (activegenre === 0) {
+    //         setFiltered(movies)
+    //     } else {
+    //         const filteredgenre = movies.filter((movie) =>
+    //           movie.genre_ids.includes(activegenre)
+    //         );
+    //         setFiltered(filteredgenre);
+    //     }
+    // }
+
+    return (
+        <>
+         <Helmet>
+            <title>BlueBird Movies | Genres</title>
+        </Helmet>
+
+        <div className='flex flex-wrap justify-center px-2'>
+            {
+                genres.map((genre) => (
+
+                    <button
+                        onClick={() => setActiveGenre(genre.id)}
+                        className={activegenre === genre.id ? 'active px-4 py-2 m-2 text-[15px] text-white font-semibold rounded-3xl' : 'px-4 py-2 m-2 text-[15px] bg-slate-800 text-white font-semibold rounded-3xl'} key={genre.id}>
+                        {genre.name}
+                    </button>
+
+                ))
+            }
+            </div>
+            </>
+    )
+}
+
+export default Genre
